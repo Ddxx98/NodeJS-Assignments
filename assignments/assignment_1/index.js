d63d18b0bbb98f04f5091e1c84d97b6aae4fbfd0
Ddxx98 <deexith2016@gmail.com>
2022-02-20 11:08:54 +0530

    commit 1

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,31 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
+    var name = process.argv
+    var n = name.length
+    return name[n-1]
 }
 
 function getNameFromEnv() {
     // Write your code here
+    // var name = process.argv
+    // var n = name.length
+    // process.env.name = name[n-1]
+    // return process.env.name
+    var Name = process.env.name
+    return Name
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline = require('readline')
+    const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+    })
+    return rl.question('',(answer) => {
+        // console.log(answer) 
+        rl.close();
+    });
 }
 
 module.exports = {
